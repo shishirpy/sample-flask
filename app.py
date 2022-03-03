02aba5c23608fdfa9d04571c3377c58869e642c6
Shishir Pandey <shishir.py@gmail.com>
2022-03-02 23:46:46 -0500

    pub-sub impl

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import zmq
 
+ctx = zmq.Context()
+pub_socket = ctx.socket(zmq.PUB)
+pub_socket.connect("ipc://sample.soc")
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    # pub_socket.send_string(request.remote_addr)
+    print(request)
+    for key in dir(request):
+        print(key, getattr(request, key))
+        pub_socket.send_string(str(getattr(request, key)))
     return render_template("index.html")
--- new_prog.py
+++ new_prog.py
@@ -2,21 +2,13 @@ import time
 import zmq
 
 ctx = zmq.Context()
-rep_socket = ctx.socket(zmq.REP)
-rep_socket.bind("ipc://sample.soc")
+sub_socket = ctx.socket(zmq.SUB)
+sub_socket.bind("ipc://sample.soc")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-req_socket = ctx.socket(zmq.REQ)
-req_socket.connect("ipc://sample.soc")
 
 count = 0
 while True:
-    req_socket.send_string(f"hi-{count}")
-
-    msg = rep_socket.recv_string()
-    print(f">>>: {msg}")
-    time.sleep(1)
-
-    rep_socket.send_string(f"bye-{count}")
-    msg_req = req_socket.recv_string()
-    print(f"***: {msg_req}")
+    msg = sub_socket.recv_string()
+    print(f"*** - {msg}")
     count += 1
